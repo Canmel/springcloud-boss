cae4e0b057d32ec3f473e1e1c9d3dec70d3c38aa
lidejian <892379244@qq.com>
2019-08-06 14:46:09 +0800

    Default Changelist

--- springcloud-activiti/src/main/java/com/camel/SpringCloudActivitiApplication.java
+++ springcloud-activiti/src/main/java/com/camel/SpringCloudActivitiApplication.java
@@ -8,7 +8,29 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
 
-// 流程
+/**
+ *
+ *                 ___====-_  _-====___
+ *           _--^^^#####//      \\#####^^^--_
+ *        _-^##########// (    ) \\##########^-_
+ *       -############//  |\^^/|  \\############-
+ *     _/############//   (@::@)   \\############\_
+ *    /#############((     \\//     ))#############\
+ *   -###############\\    (oo)    //###############-
+ *  -#################\\  / VV \  //#################-
+ * -###################\\/      \//###################-
+ *_#/|##########/\######(   /\   )######/\##########|\#_
+ *|/ |#/\#/\#/\/  \#/\##\  |  |  /##/\#/  \/\#/\#/\#| \|
+ *`  |/  V  V  `   V  \#\| |  | |/#/  V   '  V  V  \|  '
+ *   `   `  `      `   / | |  | | \   '      '  '   '
+ *                    (  | |  | |  )
+ *                   __\ | |  | | /__
+ *                  (vvv(VVV)(VVV)vvv)
+ * <工作流-ACTIVITI>
+ * @author baily
+ * @since 1.0
+ * @date 2019/8/6
+ **/
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
