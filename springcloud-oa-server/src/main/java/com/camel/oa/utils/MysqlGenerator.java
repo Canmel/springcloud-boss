109d3fad5b8ec251f56e967d3e56a0506b963a02
Camel <892379244@qq.com>
2019-10-21 14:16:03 +0800

    Integer 转 枚举类型

--- springcloud-oa-server/src/main/java/com/camel/oa/utils/MysqlGenerator.java
+++ springcloud-oa-server/src/main/java/com/camel/oa/utils/MysqlGenerator.java
@@ -59,7 +59,7 @@ public class MysqlGenerator {
                 .setDriverName("com.mysql.cj.jdbc.Driver");
         StrategyConfig strategyConfig = new StrategyConfig();
         // 指定表名
-        strategyConfig.setInclude(new String[] {"project"});
+        strategyConfig.setInclude(new String[] {"questionnaire"});
         strategyConfig
                 .setCapitalMode(true)
                 .setEntityLombokModel(false)
