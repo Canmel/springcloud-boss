9bcac35780afab5d2d5af6b377574008be2072ab
Camel <892379244@qq.com>
2019-10-22 09:18:54 +0800

    问卷

--- springcloud-oa-server/src/main/java/com/camel/oa/model/Question.java
+++ springcloud-oa-server/src/main/java/com/camel/oa/model/Question.java
@@ -1,117 +1,78 @@
 package com.camel.oa.model;
 
-import com.baomidou.mybatisplus.enums.IdType;
-import java.util.Date;
+import com.baomidou.mybatisplus.annotations.TableField;
 import com.baomidou.mybatisplus.annotations.TableId;
+import com.baomidou.mybatisplus.enums.IdType;
 import com.camel.core.entity.BasePaginationEntity;
+import com.camel.core.model.SysUser;
+import lombok.Data;
+
 import java.io.Serializable;
+import java.util.Date;
 
 /**
- *
- *                       .::::.
- *                     .::::::::.
- *                    :::::::::::
- *                 ..:::::::::::'
- *              '::::::::::::'
- *                .::::::::::
- *           '::::::::::::::..
- *                ..::::::::::::.
- *              ``::::::::::::::::
- *               ::::``:::::::::'        .:::.
- *              ::::'   ':::::'       .::::::::.
- *            .::::'      ::::     .:::::::'::::.
- *           .:::'       :::::  .:::::::::' ':::::.
- *          .::'        :::::.:::::::::'      ':::::.
- *         .::'         ::::::::::::::'         ``::::.
- *     ...:::           ::::::::::::'              ``::.
- *    ```` ':.          ':::::::::'                  ::::..
- *                       '.:::::'                    ':'````..
+ * .::::.
+ * .::::::::.
+ * :::::::::::
+ * ..:::::::::::'
+ * '::::::::::::'
+ * .::::::::::
+ * '::::::::::::::..
+ * ..::::::::::::.
+ * ``::::::::::::::::
+ * ::::``:::::::::'        .:::.
+ * ::::'   ':::::'       .::::::::.
+ * .::::'      ::::     .:::::::'::::.
+ * .:::'       :::::  .:::::::::' ':::::.
+ * .::'        :::::.:::::::::'      ':::::.
+ * .::'         ::::::::::::::'         ``::::.
+ * ...:::           ::::::::::::'              ``::.
+ * ```` ':.          ':::::::::'                  ::::..
+ * '.:::::'                    ':'````..
  * < 服务实现类>
+ *
  * @author baily
- * @since 1.0
  * @date 2019-10-21
+ * @since 1.0
  **/
+@Data
 public class Question extends BasePaginationEntity implements Serializable {
 
-private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
     /**
      * 主键
      */
-                    @TableId(value = "id", type = IdType.AUTO)
-                private Integer id;
+    @TableId(value = "id", type = IdType.AUTO)
+    private Integer id;
     /**
      * 问题标题
      */
-        private String title;
+    private String title;
     /**
      * 类型
      */
-        private Integer type;
+    private Integer type;
     /**
      * 创建时间
      */
-        private Date createdAt;
+    private Date createdAt;
     /**
-     * 创建人
+     * 创建者
      */
-        private Integer creator;
+    @TableField(value = "creator")
+    private Integer creatorId;
+
+    @TableField(exist = false)
+    private SysUser creator;
+
     /**
      * 状态
      */
-        private Integer status;
+    private Integer status;
 
-
-    public Integer getId() {
-            return id;
-            }
-
-        public void setId(Integer id) {
-            this.id = id;
-            }
-
-    public String getTitle() {
-            return title;
-            }
-
-        public void setTitle(String title) {
-            this.title = title;
-            }
-
-    public Integer getType() {
-            return type;
-            }
-
-        public void setType(Integer type) {
-            this.type = type;
-            }
-
-    public Date getCreatedAt() {
-            return createdAt;
-            }
-
-        public void setCreatedAt(Date createdAt) {
-            this.createdAt = createdAt;
-            }
-
-    public Integer getCreator() {
-            return creator;
-            }
-
-        public void setCreator(Integer creator) {
-            this.creator = creator;
-            }
-
-    public Integer getStatus() {
-            return status;
-            }
-
-        public void setStatus(Integer status) {
-            this.status = status;
-            }
-
-@Override
-public String toString() {
+    @Override
+    public String toString() {
         return "Question{" +
                 ", id=" + id +
                 ", title=" + title +
@@ -119,6 +80,6 @@ public String toString() {
                 ", createdAt=" + createdAt +
                 ", creator=" + creator +
                 ", status=" + status +
-        "}";
-        }
-        }
+                "}";
+    }
+}
