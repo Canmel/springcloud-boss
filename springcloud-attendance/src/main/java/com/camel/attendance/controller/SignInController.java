2901f969abdc96ffaeab528793a691c81cb8f924
lidejian <892379244@qq.com>
2019-11-21 14:53:53 +0800

    代码规约

---
+++ springcloud-attendance/src/main/java/com/camel/attendance/controller/SignInController.java
@@ -0,0 +1,44 @@
+package com.camel.attendance.controller;
+
+import com.camel.core.entity.Result;
+import com.camel.core.utils.ResultUtil;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ *                 ___====-_  _-====___
+ *           _--^^^#####//      \\#####^^^--_
+ *        _-^##########// (    ) \\##########^-_
+ *       -############//  |\^^/|  \\############-
+ *     _/############//   (@::@)   \\############\_
+ *    /#############((     \\//     ))#############\
+ *   -###############\\    (oo)    //###############-
+ *  -#################\\  / VV \  //#################-
+ * -###################\\/      \//###################-
+ *_#/|##########/\######(   /\   )######/\##########|\#_
+ *|/ |#/\#/\#/\/  \#/\##\  |  |  /##/\#/  \/\#/\#/\#| \|
+ *`  |/  V  V  `   V  \#\| |  | |/#/  V   '  V  V  \|  '
+ *   `   `  `      `   / | |  | | \   '      '  '   '
+ *                    (  | |  | |  )
+ *                   __\ | |  | | /__
+ *                  (vvv(VVV)(VVV)vvv)
+ * <签到>
+ * @author baily
+ * @since 1.0
+ * @date 2019/11/21
+ **/
+@RestController
+@RequestMapping("/sign")
+public class SignInController {
+    @PostMapping("/in")
+    public Result signIn() {
+        return ResultUtil.success("");
+    }
+
+    @PostMapping("/out")
+    public Result signOut() {
+        return ResultUtil.success("");
+    }
+}
