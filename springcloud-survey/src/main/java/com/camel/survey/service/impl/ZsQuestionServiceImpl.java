c2f649cd9db3602f65a0019849be5945ea072c89
lidejian <892379244@qq.com>
2020-01-09 10:46:04 +0800

    忽略配额

--- springcloud-survey/src/main/java/com/camel/survey/service/impl/ZsQuestionServiceImpl.java
+++ springcloud-survey/src/main/java/com/camel/survey/service/impl/ZsQuestionServiceImpl.java
@@ -3,34 +3,30 @@ package com.camel.survey.service.impl;
 import com.alibaba.fastjson.JSONObject;
 import com.baomidou.mybatisplus.mapper.EntityWrapper;
 import com.baomidou.mybatisplus.mapper.Wrapper;
+import com.baomidou.mybatisplus.service.impl.ServiceImpl;
 import com.baomidou.mybatisplus.toolkit.CollectionUtils;
 import com.camel.common.entity.Member;
 import com.camel.core.entity.Result;
 import com.camel.core.enums.ResultEnum;
+import com.camel.core.utils.PaginationUtil;
 import com.camel.core.utils.ResultUtil;
 import com.camel.redis.utils.SessionContextUtils;
-import com.camel.survey.annotation.AuthIgnore;
 import com.camel.survey.exceptions.SurveyNotValidException;
-import com.camel.survey.model.*;
 import com.camel.survey.mapper.ZsQuestionMapper;
+import com.camel.survey.model.*;
 import com.camel.survey.service.*;
-import com.baomidou.mybatisplus.service.impl.ServiceImpl;
-import com.camel.core.utils.PaginationUtil;
 import com.camel.survey.vo.ZsAnswerSave;
 import com.camel.survey.vo.ZsQuestionSave;
 import com.github.pagehelper.PageInfo;
 import org.apache.activemq.command.ActiveMQTopic;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.http.HttpStatus;
 import org.springframework.jms.core.JmsMessagingTemplate;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.util.ObjectUtils;
 import org.springframework.util.StringUtils;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -130,30 +126,28 @@ public class ZsQuestionServiceImpl extends ServiceImpl<ZsQuestionMapper, ZsQuest
     @Override
     @Transactional(rollbackFor = Exception.class)
     public Result saveAnswer(ZsAnswerSave zsAnswerSave) {
+        // 验证配额以及样本数量是否已经满额
         surveyService.valid(zsAnswerSave);
-        Wrapper<ZsAnswer> zsAnswerWrapper = new EntityWrapper<>();
-        zsAnswerWrapper.eq("creator", zsAnswerSave.getPhone());
-        zsAnswerWrapper.eq("survey_id", zsAnswerSave.getSurveyId());
         ZsSurvey zsSurvey = surveyService.selectById(zsAnswerSave.getSurveyId());
-        int count = answerService.selectCount(zsAnswerWrapper);
-        if (count > 0) {
+        if (isAnswered(zsAnswerSave)) {
             return ResultUtil.success(StringUtils.isEmpty(zsSurvey.getEndShow()) ? "本次访问结束，感谢您的理解和支持，再见" : zsSurvey.getEndShow());
         }
-        if(zsSurvey.isFull()) {
+        if (zsSurvey.isFull()) {
             throw new SurveyNotValidException("我们的（" + zsSurvey.getName() + "）样本个数已满，不好意思打扰您了，祝您生活愉快，再见！");
         }
-
-
         ZsAnswer zsAnswer = zsAnswerSave.buildAnswer();
         answerService.insert(zsAnswer);
+        // 获取所有问题
         List<ZsQuestion> zsQuestions = surveyService.questions(zsAnswerSave.getSurveyId());
         List<Integer> qIds = zsQuestions.stream().map(ZsQuestion::getId).collect(Collectors.toList());
+        // 获取所有问题的所有选项
         List<ZsOption> zsOptions = surveyService.options(qIds);
+        // 获取所有被选中的选项
         List<Integer> oIds = zsAnswerSave.getOptIds();
 
         List<ZsAnswerItem> zsAnswerItemList = zsAnswerSave.buildAnswerItems(zsQuestions, zsOptions, zsAnswer.getId());
 
-        if(!zsOptionService.contanisIgnore(oIds)) {
+        if (!zsOptionService.contanisIgnore(oIds)) {
             // 更新选项当前数量
             updateCurrent(oIds);
             // 当前已收集数+1
@@ -166,10 +160,34 @@ public class ZsQuestionServiceImpl extends ServiceImpl<ZsQuestionMapper, ZsQuest
         }
     }
 
+    /**
+     * 根据所选的选项判断是否需要忽略配额的增加
+     * @param zsOptions
+     * @return
+     */
+    public Boolean ignoreNum(List<ZsOption> zsOptions) {
+        Boolean flag = false;
+        for (ZsOption zsOption : zsOptions) {
+            if (zsOption.getIgnoreNum()) {
+                flag = true;
+            }
+        }
+        return flag;
+    }
+
+    /**
+     * 更新选项配额
+     * @param optionIds
+     */
     public void updateCurrent(List<Integer> optionIds) {
         zsOptionService.addOptionCurrent(optionIds);
     }
 
+    /**
+     * 使用MQ配合配额库存服务增加配额
+     * @param surveyId
+     * @param optIds
+     */
     public void updateCurrent(Integer surveyId, List<Integer> optIds) {
         JSONObject json = new JSONObject();
         json.put("surveyId", surveyId);
@@ -180,5 +198,12 @@ public class ZsQuestionServiceImpl extends ServiceImpl<ZsQuestionMapper, ZsQuest
     @Override
     public List<ZsQuestion> selectBySurveyId(Integer id) {
         return mapper.selectBySurveyId(id);
+    }
+
+    boolean isAnswered(ZsAnswerSave zsAnswerSave) {
+        Wrapper<ZsAnswer> zsAnswerWrapper = new EntityWrapper<>();
+        zsAnswerWrapper.eq("creator", zsAnswerSave.getPhone());
+        zsAnswerWrapper.eq("survey_id", zsAnswerSave.getSurveyId());
+        return answerService.selectCount(zsAnswerWrapper) > 0;
     }
 }
