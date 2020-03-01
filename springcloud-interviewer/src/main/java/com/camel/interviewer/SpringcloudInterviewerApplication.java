d75e84618b2026a5b586d0cf656dfafb0e6c1e6a
李德健 <5443863+meedesidy@user.noreply.gitee.com>
2020-03-01 11:39:02 +0800

    删除多余文件

---
+++ springcloud-interviewer/src/main/java/com/camel/interviewer/SpringcloudInterviewerApplication.java
@@ -0,0 +1,13 @@
+package com.camel.interviewer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudInterviewerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudInterviewerApplication.class, args);
+    }
+
+}
