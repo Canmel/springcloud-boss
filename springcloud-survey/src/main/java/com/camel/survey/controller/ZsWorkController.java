65036784d317e1fce86f5051ea2dfc62c4dc2fce
李德健 <5443863+meedesidy@user.noreply.gitee.com>
2020-03-16 01:14:47 +0800

    个人钱包

--- springcloud-survey/src/main/java/com/camel/survey/controller/ZsWorkController.java
+++ springcloud-survey/src/main/java/com/camel/survey/controller/ZsWorkController.java
@@ -2,18 +2,18 @@ package com.camel.survey.controller;
 
 
 import com.baomidou.mybatisplus.service.IService;
+import com.camel.core.controller.BaseCommonController;
 import com.camel.core.entity.Result;
+import com.camel.core.enums.ResultEnum;
+import com.camel.core.model.SysUser;
 import com.camel.core.utils.ResultUtil;
-import com.camel.survey.model.ZsSurvey;
 import com.camel.survey.model.ZsWork;
 import com.camel.survey.service.ZsWorkService;
-import com.camel.survey.utils.ExcelUtil;
+import com.camel.survey.utils.ApplicationToolsUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.*;
-
-import org.springframework.stereotype.Controller;
-import com.camel.core.controller.BaseCommonController;
 import org.springframework.web.multipart.MultipartFile;
 
 /**
@@ -30,6 +30,9 @@ public class ZsWorkController extends BaseCommonController {
     @Autowired
     private ZsWorkService service;
 
+    @Autowired
+    private ApplicationToolsUtils applicationUtils;
+
     @GetMapping
     public Result index(ZsWork entity, OAuth2Authentication oAuth2Authentication) {
         return ResultUtil.success(service.selectPage(entity, oAuth2Authentication));
@@ -39,6 +42,17 @@ public class ZsWorkController extends BaseCommonController {
     public Result upLoad(@RequestParam MultipartFile file) {
         service.importExcel(file);
         return ResultUtil.success("上传成功");
+    }
+
+    @GetMapping("/current")
+    public Result current(ZsWork entity, OAuth2Authentication oAuth2Authentication) {
+        SysUser sysUser = applicationUtils.currentUser();
+        if(ObjectUtils.isEmpty(sysUser.getUid()) || ObjectUtils.isEmpty(sysUser.getUsername())) {
+            return ResultUtil.error(ResultEnum.NOT_VALID_PARAM.getCode(), "对不起，清先完善身份信息");
+        }
+        entity.setIdNum(sysUser.getIdNum());
+        entity.setUname(sysUser.getUsername());
+        return ResultUtil.success(service.selectPage(entity, oAuth2Authentication));
     }
 
     @Override
