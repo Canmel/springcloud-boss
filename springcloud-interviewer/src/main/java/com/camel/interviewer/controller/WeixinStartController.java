1ddb0bc9879ac45cb97277e3290acc8b047ad4ac
李德健 <5443863+meedesidy@user.noreply.gitee.com>
2020-05-25 02:50:23 +0800

    分享

--- springcloud-interviewer/src/main/java/com/camel/interviewer/controller/WeixinStartController.java
+++ springcloud-interviewer/src/main/java/com/camel/interviewer/controller/WeixinStartController.java
@@ -26,8 +26,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Formatter;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/")
@@ -132,9 +137,14 @@ public class WeixinStartController {
 
     @AuthIgnore
     @GetMapping("/signature")
-    private Result signature(String currentUrl) {
+    private Result signature(HttpServletRequest request) {
+        String currentUrl =  request.getRequestURL().toString();
         String token = WxTokenUtil.getInstance().getTocken(wxConstants.getAppid(), wxConstants.getAppsecret(), redisTemplate);
         String ticket = JsapiTicketUtil.getInstance().JsapiTicket(token, redisTemplate);
+        Map<String, String> ret = sign(ticket, currentUrl);
+        for (Map.Entry entry : ret.entrySet()) {
+            System.out.println(entry.getKey() + ", " + entry.getValue());
+        }
         String randomStr = WxCommonsUtils.getInstance().getRandomStr();
         String timestamp = WxCommonsUtils.getInstance().timeStamp();
         Map<String, Object> map = new HashMap();
@@ -154,5 +164,63 @@ public class WeixinStartController {
     @GetMapping("getUserInfo")
     private Result getUserInfo(String code) {
         return ResultUtil.success(service.getUser(code));
+    }
+
+    public static Map<String, String> sign(String jsapi_ticket, String url) {
+        Map<String, String> ret = new HashMap<String, String>();
+        String nonce_str = create_nonce_str();
+        String timestamp = create_timestamp();
+        String string1;
+        String signature = "";
+
+        //注意这里参数名必须全部小写，且必须有序
+        string1 = "jsapi_ticket=" + jsapi_ticket +
+                "&noncestr=" + nonce_str +
+                "&timestamp=" + timestamp +
+                "&url=" + url;
+        System.out.println(string1);
+
+        try
+        {
+            MessageDigest crypt = MessageDigest.getInstance("SHA-1");
+            crypt.reset();
+            crypt.update(string1.getBytes("UTF-8"));
+            signature = byteToHex(crypt.digest());
+        }
+        catch (NoSuchAlgorithmException e)
+        {
+            e.printStackTrace();
+        }
+        catch (UnsupportedEncodingException e)
+        {
+            e.printStackTrace();
+        }
+
+        ret.put("url", url);
+        ret.put("jsapi_ticket", jsapi_ticket);
+        ret.put("nonceStr", nonce_str);
+        ret.put("timestamp", timestamp);
+        ret.put("signature", signature);
+
+        return ret;
+    }
+
+    private static String byteToHex(final byte[] hash) {
+        Formatter formatter = new Formatter();
+        for (byte b : hash)
+        {
+            formatter.format("%02x", b);
+        }
+        String result = formatter.toString();
+        formatter.close();
+        return result;
+    }
+
+    private static String create_nonce_str() {
+        return UUID.randomUUID().toString();
+    }
+
+    private static String create_timestamp() {
+        return Long.toString(System.currentTimeMillis() / 1000);
     }
 }
