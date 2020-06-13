e7cfe8cc61dc208f3042bc96e77f536e9ebbe8a5
Administrator <admin@example.com>
2020-06-13 21:57:59 +0800

    复核

---
+++ springcloud-system/src/main/java/com/camel/system/model/ZsSeat.java
@@ -0,0 +1,57 @@
+package com.camel.system.model;
+
+import com.baomidou.mybatisplus.annotations.TableField;
+import com.baomidou.mybatisplus.annotations.TableId;
+import com.baomidou.mybatisplus.enums.IdType;
+import com.camel.core.entity.BasePaginationEntity;
+import com.camel.core.model.SysUser;
+import lombok.Data;
+
+/**
+ * <p>
+ * 
+ * </p>
+ *
+ * @author baily
+ * @since 2020-02-19
+ */
+@Data
+public class ZsSeat extends BasePaginationEntity {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * 主键
+     */
+    @TableId(value = "id", type = IdType.AUTO)
+    private Integer id;
+    /**
+     * 坐席号
+     */
+    @TableField("seat_num")
+    private String seatNum;
+
+    private Integer uid;
+
+    /**
+     * 坐席密码
+     */
+    private String password;
+
+    /**
+     * 坐席描述
+     */
+    private String description;
+
+    @TableField(exist = false)
+    private SysUser user;
+
+    @Override
+    public String toString() {
+        return "ZsSeat{" +
+        ", id=" + id +
+        ", seatNum=" + seatNum +
+        ", password=" + password +
+        "}";
+    }
+}
