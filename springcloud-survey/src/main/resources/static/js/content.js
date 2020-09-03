6c81a79f892e33f2e876238a50d8ddac3af9bffa
lidejian <892379244@qq.com>
2020-09-03 09:53:31 +0800

    问卷考核去掉

--- springcloud-survey/src/main/resources/static/js/content.js
+++ springcloud-survey/src/main/resources/static/js/content.js
@@ -92,7 +92,6 @@ jQuery.validator.addMethod("isMobile", function(value, element) {
 }, "请正确填写手机号码");
 
 window.openSurvey = function () {
-    console.log('-----------uuuurrrrllll----', url);
     console.log('-----------uuuuuurrrrlllll-----', new Date());
     window.surveyStatus = 0;
     $("#surveyModal").modal('show');
