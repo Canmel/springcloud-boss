02628468df2556979e5be84617d94d1cb9e5a736
lidejian <892379244@qq.com>
2021-05-25 11:25:54 +0800

    1

---
+++ springcloud-offline/src/main/java/com/camel/offline/SpringcloudOfflineApplication.java
@@ -0,0 +1,13 @@
+package com.camel.offline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudOfflineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudOfflineApplication.class, args);
+    }
+
+}
