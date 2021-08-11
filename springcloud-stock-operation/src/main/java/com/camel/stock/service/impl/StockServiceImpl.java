d73a650dd895fd69f09eb40dcfe86b1dc9524e04
李德健 <5443863+meedesidy@user.noreply.gitee.com>
2021-08-12 00:52:33 +0800

    提交提现申请，并且可以审核。下一步发起微信支付

--- springcloud-stock-operation/src/main/java/com/camel/stock/service/impl/StockServiceImpl.java
+++ springcloud-stock-operation/src/main/java/com/camel/stock/service/impl/StockServiceImpl.java
@@ -57,19 +57,4 @@ public class StockServiceImpl implements StockService {
             namedParameterJdbcTemplate.update("update zs_option set current = current + 1 where status = 1 and id in (:optionIds)", params);
         }
     }
-
-    @Transactional(rollbackFor = RuntimeException.class)
-    @Override
-    public void add(HashMap<String, Object> map) {
-        if (map.containsKey(SURVEYID) && !ObjectUtils.isEmpty(map.get(SURVEYID))) {
-            Integer surveyId = (Integer) map.get(SURVEYID);
-            jdbcTemplate.update("update zs_survey set current_num = current_num - 1 where status = 1 and id = ? ", surveyId);
-        }
-
-        if (map.containsKey(OPTID) && !ObjectUtils.isEmpty(map.get(OPTID))) {
-            List<Integer> optIds = (List<Integer>) map.get(OPTID);
-            Map<String, Object> params = new HashMap<>(16);
-            params.put("optionIds", optIds);
-        }
-    }
 }
