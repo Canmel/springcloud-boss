4ce7f2ebd16145c4e6daba65967b007315059bd1
chenyiyang <yiyang969@126.com>
2021-08-13 09:25:10 +0800

    提交提现申请，并且可以审核。下一步发起微信支付

--- springcloud-auth-server/src/main/java/com/camel/auth/config/security/SecurityConfig.java
+++ springcloud-auth-server/src/main/java/com/camel/auth/config/security/SecurityConfig.java
@@ -71,6 +71,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .addFilterBefore(validateCodeFilter, UsernamePasswordAuthenticationFilter.class)
                 .csrf().disable();
+
     }
 
     @Override
